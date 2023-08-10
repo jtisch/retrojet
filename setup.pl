127e25d7090d43332b469abe1cda742f85d418c5
JET <jtisch@jetshome.com>
2023-08-10 19:49:28 -0400

    updates

--- setup.pl
+++ setup.pl
@@ -19,9 +19,25 @@ system("chmod +x $HOMEDIR/RetroPie/retrojet/es-*");
 
 # create symbolic links
 print "Creating symbolic links for scripts\n";
+if (! -d "$HOMEDIR/.emulationstation/scripts/game-select") {
+	mkdir ("$HOMEDIR/.emulationstation/scripts/game-select");
+}
 system("ln -s $HOMEDIR/RetroPie/retrojet/es-game-select.pl $HOMEDIR/.emulationstation/scripts/game-select/es-game-select.pl");
+
+
+if (! -d "$HOMEDIR/.emulationstation/scripts/quit") {
+        mkdir ("$HOMEDIR/.emulationstation/scripts/quit");
+}
 system("ln -s $HOMEDIR/RetroPie/retrojet/es-quit.pl $HOMEDIR/.emulationstation/scripts/quit/es-quit.pl");
+
+if (! -d "$HOMEDIR/.emulationstation/scripts/screensaver-game-select") {
+	mkdir ("$HOMEDIR/.emulationstation/scripts/screensaver-game-select");
+}
 system("ln -s $HOMEDIR/RetroPie/retrojet/es-screensaver-game-select.pl $HOMEDIR/.emulationstation/scripts/screensaver-game-select/es-screensaver-game-select.pl");
+
+if (! -d "$HOMEDIR/.emulationstation/scripts/system-select") {
+	mkdir ("$HOMEDIR/.emulationstation/scripts/system-select");
+}
 system("ln -s $HOMEDIR/RetroPie/retrojet/es-system-select.pl $HOMEDIR/.emulationstation/scripts/system-select/es-system-select.pl");
 
 exit 0;
